094c3f3c9fd2d59a4fd01fdf848e5431d2acd846
mprelee <mprelee@gmail.com>
2019-10-16 22:51:03 -0500

    Add survival.py

---
+++ bucketstats/survival.py
@@ -0,0 +1,13 @@
+# survival.py
+# Matthew Prelee
+
+__all__ = ['survival','kaplan_meier',]
+
+import numpy as np
+import pandas as pd
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
